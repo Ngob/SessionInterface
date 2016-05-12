3c7deec4f28fafa47c3e45c27238198f28f3746f
Benoit <b.ngo@thecodingmachine.com>
2016-05-12 12:00:28 +0200

    First commit

---
+++ src/SessionInterface.php
@@ -0,0 +1,15 @@
+/**
+   Author: Ngo Benoit
+**/
+
+namespace TheCodingMachine\SessionInterface;
+
+interface SessionInterface {
+
+    public function get();
+
+    public function set();
+
+    public function has();
+
+}
